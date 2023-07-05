37f3be86cbbdd56fbf83ded2a992f418c63545a0
Daniel Hutzel <daniel.hutzel@sap.com>
2023-07-05 16:43:54 +0200

    Cosmos Hackathon done right

--- cosmos-db/lib/CosmosService.js
+++ cosmos-db/lib/CosmosService.js
@@ -1,11 +1,24 @@
+const { DatabaseService } = require('@cap-js/db-service')
 const { CosmosClient } = require('@azure/cosmos')
+const cds = require ('@sap/cds/lib')
+const LOG = cds.log('cosmos|db')
 
-const SQLService = require('@cap-js/db-service')
-const cds = require('@sap/cds/lib')
-
-class CosmosService extends SQLService {
+class CosmosService extends DatabaseService {
   init() {
-    this.kind = 'cosmos-nosql'
+    this.on('READ','*', req => {
+      const [entity] = req.target.name.match(/[^.]*$/)
+      return DummyData[entity]
+    })
+    this.on('CREATE','*', req => {
+
+    })
+    this.on('UPDATE','*', req => {
+
+    })
+    this.on('DELETE','*', req => {
+
+    })
+    this.on(['BEGIN','COMMIT','ROLLBACK'], req => LOG.debug(req.event))
     return super.init(...arguments)
   }
 
@@ -21,7 +34,7 @@ class CosmosService extends SQLService {
       create: async () => {
         const cr = this.options.credentials || {}
         const credentials = {
-          endpoint: 'https://localhost:8081',
+          endpoint: 'https://hackathon-recap.documents.azure.com:443/',
           key: 'C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==',
           userAgentSuffix: 'cap'
         }
@@ -36,17 +49,9 @@ class CosmosService extends SQLService {
     }
   }
 
-  async set(variables) {}
-
-  prepare(sql) {}
-
-  exec(sql) {}
-
-  static CQN2SQL = class CQN2Comsos extends SQLService.CQN2SQL {}
-
-  async database() {}
-
-  async tenant({ tenant }) {
+  async set(variables) {} // expected by super.begin()
+  async database() {} // ???
+  async tenant({ tenant }) { // ???
     const con = await this.factory.create(this.options.credentials)
     try {
       await con.database(tenant).delete()
@@ -58,6 +63,21 @@ class CosmosService extends SQLService {
     })
     this.options.credentials.tenant = tenant
   }
+}
+
+
+const DummyData = {
+  Beers: load_csv('db/data/nosql-Beers.csv'),
+  Breweries: load_csv('db/data/nosql-Brewery.csv'),
+}
+function load_csv (filename) {
+  const { fs, path } = cds.utils
+  let src = fs.readFileSync(path.resolve(cds.root,filename),'utf8')
+  let [columns,...rows] = cds.parse.csv(src)
+  return rows.map(row => row.reduce((entry,value,i) => {
+    entry[columns[i]] = value
+    return entry
+  },{}))
 }
 
 module.exports = CosmosService
